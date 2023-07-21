31f6bda18e26f24ad90e87f5c68d3e239b783c94
Gregory Monteleone <monteleg@oregonstate.edu>
2023-07-20 21:15:10 -0400

    First GUI commit from Linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "database/database.h"
+#include "ui/mainwindow.h"
 
-int main() {
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
     // Create an instance of the DatabaseManager
     DatabaseManager data_base("audio_station_loop.db", "database/schema/schema.sql");
-    return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+
+    // return 0;
 }
