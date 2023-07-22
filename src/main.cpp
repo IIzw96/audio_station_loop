c062bd8e1fdbad5d684401081d76711fd256d82b
MonteleG <monteleg@oregonstate.edu>
2023-07-22 18:18:55 -0400

    slight changes to main.cpp and CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,10 @@
 int main(int argc, char *argv[]) {
     // Create an instance of the DatabaseManager
     DatabaseManager data_base("audio_station_loop.db", "database/schema/schema.sql");
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.show();
+    return app.exec();
 
     UI user_interface(data_base);
-
-    return 0;
 }
