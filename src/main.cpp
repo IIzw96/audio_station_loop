6fe4233b7e0c303479c4ca0ce8094b7ef57ffa79
MonteleG <monteleg@oregonstate.edu>
2023-08-03 23:15:47 -0400

    testing all changes

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,10 @@ int main(int argc, char *argv[]) {
     // Create an instance of the DatabaseManager
     DatabaseManager data_base("audio_station_loop.db", "database/schema/schema.sql");
     QApplication app(argc, argv);
-    MainWindow window;
-    window.show();
-
     UI user_interface(data_base);
 
+    MainWindow window (user_interface);
+    user_interface.load_loop(0);
+    window.show();
     return app.exec();
 }
